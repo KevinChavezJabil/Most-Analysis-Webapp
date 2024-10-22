85cead6148d1b5646c7b9fdbf4a7b321e951f264
KevinChavezJabil <Kevin_Chavez@jabil.com>
2024-10-22 11:52:02 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// constants
+const express = require('express');
+const app = express();
+const port = 3000;
+app.set('view engine', 'ejs');
+
+// render views
+app.get('/', (req, res) => {
+    console.log(__dirname)
+    res.render('index')
+});
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard')
+});
+
+app.get('/settings', (req, res) => {
+    res.render('settings')
+});
+
+app.get('/MOST', (req, res) => {
+    res.render('MOST')
+});
+
+// middleware
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    res.status(404).render('404')
+});
+
+app.listen(port, () => {
+    console.log('Server running on port', port);
+});
